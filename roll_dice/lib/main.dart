fce3e6a30d2fdb92235d82142a2414c9cd2426f2
Gülşah Düzgün <duzgun.gulsah27@gmail.com>
2025-10-27 00:04:27 +0300

    feat/styleText

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 25, 345, 34),
+                Color.fromARGB(255, 53, 42, 120),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
