231f998a578606698d48f775c93322c313f659cf
Gülşah Düzgün <duzgun.gulsah27@gmail.com>
2025-11-29 20:09:17 +0300

    adding a data model and dummy data

---
+++ quiz_app/lib/model/quiz_question.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class QuizQuestion {
+  final String questionText;
+  final List<String> answers;
+
+  const QuizQuestion(this.questionText, this.answers);
+}
