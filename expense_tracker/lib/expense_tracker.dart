de2421701d813e7f04425b78442fa476a29a3a16
Gülşah Düzgün <duzgun.gulsah27@gmail.com>
2025-12-10 23:07:13 +0300

    Efficiently Rendering Long Lists with ListView

--- expense_tracker/lib/expense_tracker.dart
+++ expense_tracker/lib/expense_tracker.dart
@@ -1,10 +1,40 @@
+import 'package:expense_tracker/expense_list.dart';
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseTracker extends StatelessWidget {
+class ExpenseTracker extends StatefulWidget {
   const ExpenseTracker({super.key});
 
   @override
+  State<ExpenseTracker> createState() {
+    return _ExpenseTrackerState();
+  }
+}
+
+class _ExpenseTrackerState extends State<ExpenseTracker> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Next.js Course",
+      amount: 29.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text("Expense Tracker")]));
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: ExpenseList(expenseList: _registeredExpenses)),
+        ],
+      ),
+    );
   }
 }
--- expense_tracker/lib/readME.md
+++ expense_tracker/lib/readME.md
@@ -1,2 +1,6 @@
 - In Dart "Initializer Lists" can be used to initialize class properties(like id) with values that are not received as constructorfunction arguments
 - Enum allows us to create custom type which simply is a combination of predefined allowed values.This alloed values defines in {} and list enum values. Dart recognizes this enums and treats these values kind od like string
+
+-Whenever we create a class automatically get a type with same name which simply contains information regarding the shape of th epbjects that will be created
+
+-If we dont know the list items lenght on Flutter dont use Column Widget, use ListView Widget. Because Column tries to get all data at once but the ListView Widget gets a scrollable list which still creates all items immediately when this list is diplayed on the scree.The builder constructor with ListView Widget tells Flutter to create scrollable Widget but create those items only if they are visible or about to become visible
