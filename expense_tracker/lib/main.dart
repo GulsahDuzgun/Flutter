385c54b6ca4e363eeeb888a8c557d6c57e85ab91
Gülşah Düzgün <duzgun.gulsah27@gmail.com>
2025-12-29 23:47:08 +0300

    adding dark mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -5,9 +5,32 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkThemeScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark,
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        cardTheme: const CardThemeData().copyWith(
+          color: kDarkThemeScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkThemeScheme.primaryContainer,
+            foregroundColor: kDarkThemeScheme.onPrimaryContainer,
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkThemeScheme.primaryContainer,
+            foregroundColor: kDarkThemeScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -32,6 +55,7 @@ void main() {
         ),
       ),
       home: ExpenseTracker(),
+      themeMode: ThemeMode.dark,
     ),
   );
 }
