73bffd013689afb7896893c4fee63cc26b3380c7
Gülşah Düzgün <duzgun.gulsah27@gmail.com>
2025-12-29 20:55:19 +0300

    setting & using Color scheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:expense_tracker/widgets/expense_tracker.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(home: ExpenseTracker()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: ExpenseTracker(),
+    ),
+  );
 }
